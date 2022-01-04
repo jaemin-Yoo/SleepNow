7882f8c4fbf2b837710ac024ac936248f37f6aa9
jaemin-Yoo <iamjm29@naver.com>
2022-01-04 13:16:11 +0900

    Init Commit

---
+++ SleepNow/app/src/main/java/com/example/sleepnow/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.sleepnow
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME: Long = 3000 // (ms)초 간 Splash 화면 띄움
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME)
+    }
+}
