377bc752f55f603d4dc0bd671253bd89afb902c3
jaemin-Yoo <iamjm29@naver.com>
2022-01-05 16:39:43 +0900

    Add font

--- SleepNow/app/src/main/java/com/example/sleepnow/dialogs/CustomDialog.kt
+++ SleepNow/app/src/main/java/com/example/sleepnow/dialogs/CustomDialog.kt
@@ -7,6 +7,7 @@ import com.example.sleepnow.R
 import com.example.sleepnow.data.SleepTimeData
 import com.example.sleepnow.viewmodels.MainViewModel
 import kotlinx.android.synthetic.main.dialog_time_list.*
+import java.text.SimpleDateFormat
 
 class CustomDialog(context: Context){
     private val dialog = Dialog(context)
@@ -14,15 +15,27 @@ class CustomDialog(context: Context){
     fun showDialog(sleepTimeList: ArrayList<SleepTimeData>){
         dialog.setContentView(R.layout.dialog_time_list)
 
+        // String to Date, ex) time = "12:00"
+        var dateParse = SimpleDateFormat("hh:mm")
+        var time1 = dateParse.parse(sleepTimeList[0].time)
+        var time2 = dateParse.parse(sleepTimeList[1].time)
+        var time3 = dateParse.parse(sleepTimeList[2].time)
+        var time4 = dateParse.parse(sleepTimeList[3].time)
+
         // dialog 셋팅
-        dialog.tv_time1.text = sleepTimeList[0].time
-        dialog.tv_time2.text = sleepTimeList[1].time
-        dialog.tv_time3.text = sleepTimeList[2].time
-        dialog.tv_time4.text = sleepTimeList[3].time
+        var dateFormat = SimpleDateFormat("hh:mm a")
+        dialog.tv_time1.text = dateFormat.format(time1)
+        dialog.tv_time2.text = dateFormat.format(time2)
+        dialog.tv_time3.text = dateFormat.format(time3)
+        dialog.tv_time4.text = dateFormat.format(time4)
 
         dialog.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT) // 크기 설정
         dialog.setCanceledOnTouchOutside(true) // 영역 밖 클릭 시 다이얼로그 사라짐
         dialog.setCancelable(true) // Back키로 다이얼로그 닫기
         dialog.show()
+
+        dialog.btn_exit.setOnClickListener {
+            dialog.dismiss()
+        }
     }
 }
--- SleepNow/app/src/main/java/com/example/sleepnow/viewmodels/SelectAgeViewModel.kt
+++ SleepNow/app/src/main/java/com/example/sleepnow/viewmodels/SelectAgeViewModel.kt
@@ -6,13 +6,19 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 
 class SelectAgeViewModel(application: Application): AndroidViewModel(application) {
-    var age = MutableLiveData<Int>()
+    var age = MutableLiveData<String>()
 
     init{
-        age.value = 1
+        age.value = "1살"
     }
 
     fun changeAge(seekBar: SeekBar){
-        age.value = seekBar.progress
+
+        var progress = seekBar.progress
+        if (progress >= 65){
+            age.value = "${progress}+살"
+        } else{
+            age.value = "${progress}살"
+        }
     }
 }
